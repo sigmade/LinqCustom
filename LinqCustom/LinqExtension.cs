2b2f74b2b40066d39de81dca6f1c16abcb5db81f
Egor <sychyov1991@mail.ru>
2022-03-24 20:50:33 +0600

    Add project files.

---
+++ LinqCustom/LinqExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LinqCustom
+{
+    public static class LinqExtension
+    {
+        public static int CustomSum(this IEnumerable<int> source)
+        {
+            var sum = 0;
+
+            foreach (var item in source)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static int CustomMax(this IEnumerable<int> source) 
+        { 
+            var maxValue = 0;
+
+            foreach (var item in source)
+            {
+                if (item > maxValue)
+                {
+                    maxValue = item;
+                }
+            }
+
+            return maxValue;
+        }
+    }
+}
---
+++ LinqCustom/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqCustom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, -2 };
+
+            var sum = numbers.CustomMax();
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
